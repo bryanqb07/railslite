a2efd95ff5ebdf5d35b6997594f0bf8184311051
Bryan Lynch <bml312@nyu.edu>
2019-04-08 14:25:27 -0400

    add session

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -20,32 +20,46 @@ class ControllerBase
 
   # Set the response status code and header
   def redirect_to(url)
-    raise "Response already built" if already_built_response?
+    prepare_render_redirect
     @res.status = 302
     @res['Location'] = url
-    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    raise "Response already built" if already_built_response?
+    prepare_render_redirect
     @res['Content-Type'] = content_type
     @res.write(content)
-    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    # "skeleton/views/#{controller_name}/#{template_name}.html.erb"
+    dir_path = File.dirname(__FILE__) # dir of this current file /skeleton/lib/
+    # .. = jump back one directory
+    template_f = File.join(dir_path, "..", "views",
+      self.class.name.underscore, "#{template_name}.html.erb")
+    template_code = File.read(template_f)
+    #bind file into an ERB object
+    render_content(ERB.new(template_code).result(binding), "text/html")
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+  end
+
+  private
+  def prepare_render_redirect
+      raise "Double render error" if already_built_response?
+      session.store_session(@res)
+      @already_built_response = true
   end
 end
