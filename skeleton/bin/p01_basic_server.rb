16d5747c6e82f1043c048c74968a7e4b882fb3b2
Bryan Lynch <bml312@nyu.edu>
2019-04-08 12:13:34 -0400

    pass controller spec

---
+++ skeleton/bin/p01_basic_server.rb
@@ -0,0 +1,15 @@
+require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  path = req.path_info
+  res.write(path)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
